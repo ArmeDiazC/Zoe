8a241db8b9e399445f78a887fb746d8e5672f171
armeDiazC <armediazc@gmail.com>
2022-05-02 18:33:29 -0500

    Zoe_Financial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.scss";
 
 function App() {
+  const [income, setIncome] = useState("");
+  const [error, setError] = useState(false);
+  const [filtered, setFiltered] = useState([]);
+
+  const [counter, setCounter] = useState(3);
+
+  const handleInput = (e) => {
+    const value = e.target.value;
+
+    if (value.length >= 6) {
+      setError(true);
+    } else {
+      setError(false);
+      setIncome(value);
+    }
+  };
+
+  const handleRequest = async () => {
+    await fetch("/AGENTS_LIST.json")
+      .then((resp) => resp.json())
+      .then((data) => {
+        filterAgents(data);
+      });
+  };
+
+  const filterAgents = (data) => {
+    const listAgents = data.filter((agent) => {
+      let diff = Math.abs(agent.income - income);
+
+      if (diff > 10000) {
+        return false;
+      } else {
+        return true;
+      }
+    });
+    console.log(listAgents);
+    setFiltered(listAgents);
+  };
+
+  const handleCounter = (operation) => {
+    let indexView = operation === "more" ? counter + 3 : counter - 3;
+
+    indexView =
+      indexView > filtered.length
+        ? filtered.length
+        : indexView < 3
+        ? 3
+        : indexView;
+
+    setCounter(indexView);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" onChange={handleInput} />
+      {error && <>The income should have 5 digits</>}
+      <button onClick={handleRequest} disabled={error}>
+        Get matches
+      </button>
+      {filtered &&
+        filtered
+          .map((agent) => (
+            <div key={agent.id} className="card">
+              <img src="" alt="" />
+              <div>{agent.name}</div>
+              <div>{agent.id}</div>
+              <div>{agent.income}</div>
+            </div>
+          ))
+          .slice(0, counter)}
+      <button onClick={() => handleCounter("more")}>show More</button>
+      <button onClick={() => handleCounter("less")}>show Less</button>
     </div>
   );
 }
